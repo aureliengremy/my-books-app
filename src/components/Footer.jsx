f4b96450a6bc524cf1662c7b589d81b250919211
Aurelien Gremy <gremy.aurelien@gmail.com>
2023-01-26 18:17:43 -0500

    feat(Footer) create component w/style

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="text-white bg-slate-800 mt-auto">
+        <div className="container p-3">
+            <span>&copy; AurelienGremy</span>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
